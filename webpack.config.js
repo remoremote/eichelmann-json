620e398157a56a08140204a6bca3d8e858d8459e
Remo <81964236+remoremote@users.noreply.github.com>
2024-03-06 09:59:17 +0000

    comma

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,12 @@ module.exports = {
   mode: 'production',
   entry: './netlify/functions/start-scraper.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), // The output directory
+    path: path.resolve(__dirname, 'dist'), // The corrected output directory
     filename: '[name].bundle.js',
-  }
+  }, // Added missing comma here
   resolve: {
     fallback: { "path": false, "fs": false },
     alias: {
-      // Define an alias '@root' to refer to the project root directory
       '@root': path.resolve(__dirname),
     }
   },
