77625fd30fa8d2ab7dfa0f85d106d9244ab55705
Remo <81964236+remoremote@users.noreply.github.com>
2024-03-07 11:10:44 +0000

    special button

--- netlify/functions/start-scraper.js
+++ netlify/functions/start-scraper.js
@@ -1,10 +1,15 @@
 // netlify/functions/start-scraper.js
 const { main } = require('../../index.js');
+const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
   try {
     // Invoke the main function from your project's entry point
     await main();
+
+    // Update button text
+    await updateButtonText('Scraping Completed');
+
     // Respond with a success message
     return {
       statusCode: 200,
@@ -19,3 +24,24 @@ exports.handler = async function (event, context) {
     };
   }
 };
+
+async function updateButtonText(buttonText) {
+  try {
+    const response = await fetch('https://eichelmann.netlify.app/update-button-text', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ buttonText }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to update button text');
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error updating button text:', error.message);
+  }
+}
